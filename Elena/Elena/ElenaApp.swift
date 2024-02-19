4f369316a774e0bc005fc402950415e8843395a7
aalekseenko23 <148762775+aalekseenko23@users.noreply.github.com>
2024-02-19 10:53:38 +0100

    Let's get started

---
+++ Elena/Elena/ElenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElenaApp.swift
+//  Elena
+//
+//  Created by Alekseenko Artem   on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ElenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
