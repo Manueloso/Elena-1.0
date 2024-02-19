47ad8053f31bb8a162d522ed8e2c454fdd3e5909
aalekseenko23 <148762775+aalekseenko23@users.noreply.github.com>
2024-02-19 10:44:35 +0100

    Add files via upload

---
+++ Script/Script/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Script
+//
+//  Created by Alessandro Ricci on 16/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "sun")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("prova")
+            Text("Pall")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
